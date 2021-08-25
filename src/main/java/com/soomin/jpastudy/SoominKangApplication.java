bc927daf480d81ed2c86a05e76a206804bc071d4
강수민 <soomin_kang@tmax.co.kr>
2021-08-25 15:34:37 +0900

    Init Project

---
+++ src/main/java/com/soomin/jpastudy/SoominKangApplication.java
@@ -0,0 +1,13 @@
+package com.soomin.jpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoominKangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoominKangApplication.class, args);
+	}
+
+}
