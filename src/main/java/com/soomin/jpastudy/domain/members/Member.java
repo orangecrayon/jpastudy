323c3a89a9859a6b5cf5484cf746d50fdc6b4de8
soomin_kang <oorangecrayon@gmail.com>
2021-09-02 14:49:15 +0900

    feat: 양방향 맵핑, ManyToOne, OneToMany

--- src/main/java/com/soomin/jpastudy/domain/members/Member.java
+++ src/main/java/com/soomin/jpastudy/domain/members/Member.java
@@ -5,13 +5,11 @@ import lombok.*;
 import javax.persistence.*;
 
 @Getter
-@Setter
-@Entity
-@Table(name = "MEMBER")
 @Builder
 @NoArgsConstructor
-@RequiredArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "MEMBER")
 public class Member {
     @Id
     @Column(name = "ID")
@@ -25,7 +23,19 @@ public class Member {
 
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
+    // 데이터베이스에서는 FK를 설정할 때, 항상 Many인 쪽이 FK임.
+    // 즉 ManyToOne, OneToMany 양방향 관계에서 Many쪽이 FK 이고 연관관계의 주인
+    // 연관관계의 주인은 항상 FK가 있는 쪽.
     private Team team;
+
+    public void setTeam(Team team) {
+        if(this.team != null) {
+            this.team.getMembers().remove(this);
+        }
+
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
     @Override
     public String toString() {
--- src/main/java/com/soomin/jpastudy/domain/members/Team.java
+++ src/main/java/com/soomin/jpastudy/domain/members/Team.java
@@ -5,17 +5,28 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
-@Setter
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Team {
     @Id
     @Column(name = "TEAM_ID")
     private String id;
 
     private String name;
+
+    @Setter
+    @OneToMany(mappedBy = "team")
+    // mappedBy = 양방향 매핑이 되는 반대쪽 필드 이름, mappedBy 로 지정된 쪽이 연관관계의 주인
+    // 주인쪽에서 FK를 관리하기 때문에 주인이 아닌 이곳에서 FK 등록을 하려고하면 실패함
+    // team.getMembers().add(member1)  <-- 이런코드는 주인이 아닌쪽에서 멤버를 등록하려고하기 때문에 안됨
+    // member1.setTeam(team) <-- 주인인 member에서 team을 등록하는것만 가능
+    private List<Member> members = new ArrayList<Member>();
+
 }
